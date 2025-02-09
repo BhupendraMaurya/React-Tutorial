4894a30ad314e8af9b3293320ce9d60742e8adf8
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-02-09 10:44:00 +0530

    Adding Multiple child to single element

--- Episode 1/part 3/app.js
+++ Episode 1/part 3/app.js
@@ -1,12 +1,16 @@
 /* <div id="parent">
         <div id="child">
             <h1>I am h1 tag dost</h1>
+            <h2> I am h2 tag bro </h2>
         </div>
     </div> */
 
+// if there are multiple child to any element,then put that in an array
 const parent = React.createElement("div", {id:"parent"},
     React.createElement("div",{id:"child"},
-        React.createElement("h1",{},"I am h1 tag dost by nested ")
+        [React.createElement("h1",{},"I am h1 tag dost by nested "),
+            React.createElement("h1",{},"I am h1 tag dost by nested ")
+        ]
     )
 );
 
