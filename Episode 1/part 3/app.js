11ebfdbcdc4b7fcf59247ce49a9191100e900dde
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-02-09 10:54:43 +0530

    Adding multiple child using Array

--- Episode 1/part 3/app.js
+++ Episode 1/part 3/app.js
@@ -3,15 +3,25 @@
             <h1>I am h1 tag dost</h1>
             <h2> I am h2 tag bro </h2>
         </div>
+
+        <div id="child2">
+            <h3>I am h1 tag dost</h3>
+            <h4> I am h2 tag bro </h4>
+        </div>
     </div> */
 
 // if there are multiple child to any element,then put that in an array
 const parent = React.createElement("div", {id:"parent"},
-    React.createElement("div",{id:"child"},
+    [React.createElement("div",{id:"child"},
         [React.createElement("h1",{},"I am h1 tag dost by nested "),
             React.createElement("h1",{},"I am h1 tag dost by nested ")
         ]
-    )
+    ), 
+    React.createElement("div",{id:"child2"},
+        [React.createElement("h3",{},"I am h1 tag dost by nested "),
+            React.createElement("h4",{},"I am h1 tag dost by nested ")
+        ]
+    )]
 );
 
 console.log(parent);
