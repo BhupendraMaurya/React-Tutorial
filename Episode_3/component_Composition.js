2f0873641c31103ceac4bc21aa9f8f1179e5ecfa
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-02-16 08:50:25 +0530

    Adding component composition file

--- Episode_3/component_Composition.js
+++ Episode_3/component_Composition.js
@@ -1,21 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () =>{
-    <h1 className="head" tabIndex="5">
-        Namaste React using the JSX
-    </h1>
-};
+// const Title = () =>{
+//     <h1 className="head" tabIndex="5">
+//         Namaste React using the JSX
+//     </h1>
+// };
 
-// Component Composition =>Putting one React element / JSX code inside another.
-const HeadingComponent = () =>{
+// // WE can also write JS inside JSX, and it is a very powerfull thing
+
+// // Component Composition =>Putting one React element / JSX code inside another.
+// const HeadingComponent = () =>{
+//     <div id="container">
+//         <Title />
+//         <h1 className="heading"> Namaste react functional Component</h1>
+//     </div>
+// };
+
+const elem = <span>React Element</span>
+// const title = (
+//     <h1 className="head" tabIndex="5">
+//         {elem}
+//         Namaste React using the JSX
+        
+//     </h1>
+// );
+
+// // WE can also write JS inside JSX, and it is a very powerfull thing
+
+// // Component Composition =>Putting one React element / JSX code inside another.
+// const HeadingComponent = () => (
+//     <div id="container">
+         
+//         <h2>{ 100 + 400 } </h2>
+
+//         {title}
+//         <h1 className="heading"> Namaste react functional Component</h1>
+//     </div>
+// );
+
+
+const HeadingComponent = () => (
     <div id="container">
-        <Title />
+         
         <h1 className="heading"> Namaste react functional Component</h1>
     </div>
-};
+);
 
-
+const title = (
+    
+    <h1 className="head" tabIndex="5">
+        
+        Namaste React using the JSX
+        <HeadingComponent />
+    </h1> 
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(title);
