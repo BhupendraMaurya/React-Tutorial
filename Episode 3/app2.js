62f7d4caee2d36abe1e95acbac0c15c94a39641d
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2025-02-23 11:25:32 +0530

    Added multiple carts and applied some CSS on them

--- Episode 3/app2.js
+++ Episode 3/app2.js
@@ -9,7 +9,9 @@ import ReactDOM from "react-dom/client";
 - Body
 1. Search
 2. RestaurantContainer
-3. RestaurantCard
+3. RestaurantCard 
+   - IMG
+   - Name of restaurant, star rating, quisins, etc., delivery time  
 
 - Footer
 1. Copyright
@@ -37,13 +39,63 @@ const Header = () =>{
         </div>
     )
 }
+
+// const styleCard = {
+//     backgroundColor : "#f0f0f0"
+// }
+const RestaurantCard = () =>{
+    return (
+        <div className="res-card" style={{ backgroundColor : "#f0f0f0" }}>
+            <img 
+            className="res-logo"
+            alt="res-logo" 
+            width="30px" src="https://imgs.search.brave.com/1qo0fZptaYZQK-qmcT7tEyKnaWnVp4GmAi2d_cObzRU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jLm5k/dHZpbWcuY29tLzIw/MjUtMDIvY21qa3F0/cThfdmlyYWwtdmlk/ZW8tc2hvd3Mtd29y/bXMtaW4tdGhlLWdv/b2QtYm93bC1tZWFs/XzYyNXgzMDBfMTRf/RmVicnVhcnlfMjUu/anBlZz9pbT1GYWNl/Q3JvcCxhbGdvcml0/aG09ZG5uLHdpZHRo/PTc3MyxoZWlnaHQ9/NDM1"/>
+            <h3> Meghana Foods</h3>
+
+            <h4>Biryano, North Indian, Asian </h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes </h4>
+        </div>   
+    );
+};
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+              
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+               <RestaurantCard />
+              
+            </div>
+        </div>
+    );
+};
 const AppLayout = () =>{
     return (
         <div className="app">
            <Header />
+
+           <Body />
         </div>
-    )
-}
+    ); 
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
